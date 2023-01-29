2d52c6e4b16cb562bf2185f95c11d6eec2d42b7e
kondratievm <mkwtzt@gmail.com>
2023-01-29 13:56:26 +0400

    fix logic

--- specs/weightCalculator.spec.js
+++ specs/weightCalculator.spec.js
@@ -1,50 +1,54 @@
-const {
-  describe,
-  test,
-  beforeEach,
-  afterEach,
-  expect,
-} = require("@jest/globals");
+const { describe, test, expect, afterAll } = require("@jest/globals");
 const { chromium } = require("playwright");
 
 const { MainPage } = require("./framework");
 
 jest.setTimeout(30000);
 
-const CORRECT_VOLUMETRIC_WEIGHT = Math.round(Math.max(92.6 / 5, 12) * 10) / 10;
-const CORRECT_TOTAL_WEIGHT = Math.round(((122 * 23 * 33) / 1000) * 10) / 10;
+function correct_volume(length, width, height) {
+  return Math.round(((length * width * height) / 1000) * 10) / 10;
+}
+
+function correct_total_weight(volume, weight) {
+  return Math.round(Math.max(volume / 5, weight) * 10) / 10;
+}
 
 describe("Weight calculator capacity", () => {
   let browser;
   let mainPage;
   let page;
 
-  beforeEach(async () => {
-    browser = await chromium.launch({ headless: true });
+  beforeAll(async () => {
+    browser = await chromium.launch({ headless: false });
 
     const context = await browser.newContext();
     page = await context.newPage();
 
     await page.goto("https://calculator.ozon.ru/");
-
-    mainPage = new MainPage(page);
-    await mainPage.clickDimesionsBtn();
-    await mainPage.fillDimensionsInputs("122", "23", "33", "12");
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await browser.close();
   });
 
-  test("check volumetric product weight", async () => {
-    const volumetricWeight = await mainPage.getVolumetricWeght();
+  test.each([
+    ["122", "23", "33", "12"],
+    ["1", "23", "3", "12"],
+  ])(
+    "check volumetric product weight and total weight",
+    async (length, width, height, weight) => {
+      mainPage = new MainPage(page);
 
-    expect(CORRECT_VOLUMETRIC_WEIGHT === volumetricWeight).toBe(true);
-  });
+      await mainPage.clickDimesionsBtn();
+      await mainPage.fillDimensionsInputs(length, width, height, weight);
 
-  test("check total product weight", async () => {
-    const totalWeight = await mainPage.getTotalWeight();
+      const volumetricWeight = await mainPage.getVolumetricWeght();
+      const expectedVolume = correct_volume(length, width, height);
+      const totalWeight = await mainPage.getTotalWeight();
+      const expectedWeight = correct_total_weight(expectedVolume, weight);
 
-    expect(CORRECT_TOTAL_WEIGHT === totalWeight).toBe(true);
-  });
+      expect(expectedVolume).toBe(totalWeight);
+      expect(expectedWeight).toBe(volumetricWeight);
+    }
+  );
 });
